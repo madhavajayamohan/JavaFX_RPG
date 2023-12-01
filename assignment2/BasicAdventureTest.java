f3323edee0c414590b8e682d61b64fd0c1212f59
NdaruBhramastra <ndarubhramastra@gmail.com>
2023-12-01 14:38:01 -0500

    [DEV-2.1]

--- assignment2/BasicAdventureTest.java
+++ assignment2/BasicAdventureTest.java
@@ -1,7 +1,9 @@
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import AdventureModel.AdventureGame;
+import AdventureModel.Room;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +21,18 @@ public class BasicAdventureTest {
         AdventureGame game = new AdventureGame("TinyGame");
         String objects = game.player.getCurrentRoom().getObjectString();
         assertEquals("a water bird", objects);
+    }
+
+    void properObjectDescTest() throws IOException {
+        AdventureGame game = new AdventureGame("TinyGame");
+        HashMap<Integer, Room> rooms = game.getRooms();
+        assertEquals("a water bird", rooms.get(1).objectsInRoom.get(0).getDescription());
+    }
+    @Test
+    void properObjectDescTest2() throws IOException {
+        AdventureGame game = new AdventureGame("TinyGame");
+        HashMap<Integer, Room> rooms = game.getRooms();
+        assertEquals("a pirate chest", rooms.get(2).objectsInRoom.get(0).getDescription());
     }
 
 
